34a88af3a1b7b91fe21ed39490bc316440993b9c
arcnmx <git@git.arcn.mx>
2022-04-14 18:28:02 -0700

    ci

---
+++ ci/config.nix
@@ -0,0 +1,23 @@
+{ pkgs, lib, ... }: with lib; let
+  flake-check = name: path: pkgs.ci.command {
+    name = "${name}-check";
+    resolverPath = toString ./resolver;
+    libPath = toString ./lib;
+    pkgs = toString pkgs.path;
+    command = ''
+      if [[ $CI_PLATFORM = gh-actions ]]; then
+        nix registry add github:NixOS/nixpkgs path:$pkgs
+      fi
+      nix flake check ./${path}
+    '';
+    impure = true;
+    environment = [ "CI_PLATFORM" ];
+  };
+in {
+  name = "flakes.nix";
+  ci.version = "nix2.4-broken";
+  ci.gh-actions.enable = true;
+  cache.cachix.arc.enable = true;
+  channels.nixpkgs = "21.11";
+  tasks.flakes.inputs = map (flake: flake-check "${flake}" "./${flake}") (importJSON ./flakes.json);
+}
