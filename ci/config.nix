cdfc3cd543c85ee50a5c1d9f5096828eeaa5a8d4
arcnmx <git@git.arcn.mx>
2022-04-28 12:35:58 -0700

    fix broken check

--- ci/config.nix
+++ ci/config.nix
@@ -3,25 +3,32 @@
     inherit name;
     resolverPath = toString ./resolver;
     libPath = toString ./lib;
-    pkgs = toString pkgs.path;
-    command = ''
-      if [[ $CI_PLATFORM = gh-actions ]]; then
-        nix registry add github:NixOS/nixpkgs path:$pkgs
-      fi
-      nix ${command}
-    '';
+    command = "nix ${command}";
     impure = true;
-    environment = [ "CI_PLATFORM" ];
-    meta.broken = name == "base16";
+    meta.broken = name == "base16-check";
   };
   flake-check = name: path: nix "${name}-check" "flake check ./${path}";
+  flakeNames = importJSON ./flakes.json;
 in {
   name = "flakes.nix";
   ci.version = "nix2.4-broken";
   ci.gh-actions.enable = true;
-  cache.cachix.arc.enable = true;
+  cache.cachix.arc = {
+    enable = true;
+    publicKey = "arc.cachix.org-1:DZmhclLkB6UO0rc0rBzNpwFbbaeLfyn+fYccuAy7YVY=";
+  };
+
   channels.nixpkgs = "21.11";
-  tasks.flakes.inputs = map (flake: flake-check "${flake}" "./${flake}") (importJSON ./flakes.json) ++ [
-    (nix "base16-build" "build ./base16#base16-schemes")
-  ];
+  tasks = {
+    flakes = {
+      preBuild = ''
+        if [[ $CI_PLATFORM = gh-actions ]]; then
+          nix registry add github:NixOS/nixpkgs path:${toString pkgs.path}
+        fi
+      '';
+      inputs = map (flake: flake-check flake "./${flake}") flakeNames ++ [
+        (nix "base16-build" "build ./base16#base16-schemes")
+      ];
+    };
+  };
 }
