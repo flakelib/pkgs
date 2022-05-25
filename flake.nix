7d4ceeb195024083caf4d7ec20bd6ab7366d2ffa
arcnmx <git@git.arcn.mx>
2022-05-24 20:16:22 -0700

    base16 fixes

--- flake.nix
+++ flake.nix
@@ -3,18 +3,18 @@
   inputs = {
     flakelib.url = "github:flakelib/fl";
     nixpkgs.url = "github:NixOS/nixpkgs";
-    arclib = {
-      url = "../lib";
-      inputs = {
-        flakelib.follows = "flakelib";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
   };
   outputs = { flakelib, ... }@inputs: flakelib {
     inherit inputs;
+    config = {
+      name = "base16";
+    };
+    lib = import ./lib;
     builders = import ./builders.nix;
     packages = import ./packages;
+    nixosModules = import ./nixos.nix;
+    outputs.homeModules.value = import ./home.nix;
+    outputs.modules.value.base16 = import ./module.nix;
     legacyPackages = {
       base16-templates = import ./packages/templates.nix;
     };
---
+++ nixos.nix
@@ -0,0 +1,53 @@
+{ inputs'self'modules'base16, lib'base16 }: { pkgs, config, lib, ... }: let
+  cfg = config.base16;
+  base16 = lib.base16 or lib'base16;
+  toHexUpper = throw "TODO";
+  concatImap0Strings = throw "TODO";
+  makeColorCS = n: value: "\\e]P${toHexUpper n}${value}";
+in with lib; {
+  imports = [
+    (import ./home.nix { inherit inputs'self'modules'base16 lib'base16; isNixos = true; })
+  ];
+
+  options.console.getty = {
+    greetingPrefix = mkOption {
+      type = types.separatedString "";
+      default = "";
+    };
+    greeting = mkOption {
+      type = types.str;
+    };
+  };
+  options.base16 = {
+    console = {
+      enable = mkEnableOption "base16 Linux console colours";
+      schemeName = mkOption {
+        type = types.str;
+        default = cfg.defaultSchemeName;
+      };
+      scheme = mkOption {
+        type = with types; coercedTo str (name: cfg.schemes.${name}) attrs;
+        default = cfg.schemes.${cfg.console.schemeName};
+      };
+      getty = {
+        enable = mkEnableOption "getty login colours" // {
+          default = cfg.console.enable;
+          defaultText = "true";
+        };
+      };
+      ansiCompatibility = mkEnableOption "bright colours mimic their normal counterparts" // { default = cfg.terminal.ansiCompatibility; };
+    };
+  };
+  config = {
+    console = mkIf cfg.console.enable {
+      colors = map (b: b.rgb) (sort (a: b: a.ansiIndex < b.ansiIndex) (attrValues (getAttrs base16.names cfg.console.scheme)));
+      getty = mkIf cfg.console.getty.enable {
+        greetingPrefix = mkBefore (concatImap0Strings makeColorCS config.console.colors);
+        greeting = mkDefault ''<<< Welcome to NixOS ${config.system.nixos.label} (\m) - \l >>>'';
+      };
+    };
+    services.getty = mkIf cfg.console.getty.enable {
+      greetingLine = "${config.console.getty.greetingPrefix}${config.console.getty.greeting}";
+    };
+  };
+}
