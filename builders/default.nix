cc38b34a42a23c940231e1911b3e5686b689e541
arcnmx <git@git.arcn.mx>
2025-08-23 15:07:32 -0700

    migrate builders over

--- builders/default.nix
+++ builders/default.nix
@@ -1,6 +1,12 @@
 {
   __functor = _: { callPackageSet, callPackage, nixpkgs }: {
     nodeEnv = callPackage (nixpkgs.outPath + "/pkgs/development/node-packages/node-env.nix") { };
-  } // callPackageSet (import ./yggdrasil.nix) { };
+  } // callPackageSet (
+    import ./curl.nix
+    // import ./fetchurl.nix
+    // import ./shell.nix
+    // import ./wrap.nix
+    // import ./yggdrasil.nix
+  ) { };
   fl'config.args.nixpkgs.components = [ "inputs" "nixpkgs" ];
 }
